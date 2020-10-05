694e0c79dc653e0794d8518ed15e392d2da43a52
Nico <46602571+Nicopdev@users.noreply.github.com>
2020-10-05 12:24:25 +0200

    Update brain.py

--- brain.py
+++ brain.py
@@ -1,14 +1,18 @@
+# -*- coding: utf-8 -*-
+
 from models import Solver
 
 solver = Solver('/dev/cu.usbmodem14101')
-solver.solve('DRLUUBFBRBLURRLRUBLRDDFDLFUFUFFDBRDUBRUFLLFDDBFLUBLRBD')
+solver.solve('DRLUUBFBRBLURRLRUBLRDDFDLFUFUFFDBRDUBRUFLLFDDBFLUBLRBD') # This string represents the scrumbled cube (see https://pypi.org/project/kociemba/)
 
 def main():
+    
+    # Get a feedback since no hardware is currently connected to the Arduino
     result = ''
-    while 1:
+    while True:
         while solver.port.waiting():
             result += solver.port.get().decode('ascii')
-            print(result.replace('\n', ' '))
-
+            print(result.replace('\n', ''))
+            
 if __name__ == '__main__':
     main()
